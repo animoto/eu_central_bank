209b7c0f0936ac474ae0bdfdd6f6837157bcfdf9
Jon Eisenstein <jon@animoto.com>
2012-01-10 13:35:51 -0500

    Add last_updated and export/import from XML string.

--- lib/eu_central_bank.rb
+++ lib/eu_central_bank.rb
@@ -7,16 +7,13 @@ class InvalidCache < StandardError ; end
 
 class EuCentralBank < Money::Bank::VariableExchange
 
+  attr_accessor :last_updated
+
   ECB_RATES_URL = 'http://www.ecb.int/stats/eurofxref/eurofxref-daily.xml'
   CURRENCIES = %w(USD JPY BGN CZK DKK GBP HUF LTL LVL PLN RON SEK CHF NOK HRK RUB TRY AUD BRL CAD CNY HKD IDR INR KRW MXN MYR NZD PHP SGD THB ZAR)
 
   def update_rates(cache=nil)
-    exchange_rates(cache).each do |exchange_rate|
-      rate = exchange_rate.attribute("rate").value.to_f
-      currency = exchange_rate.attribute("currency").value
-      add_rate("EUR", currency, rate)
-    end
-    add_rate("EUR", "EUR", 1)
+    update_parsed_rates(exchange_rates(cache))
   end
 
   def save_rates(cache)
@@ -26,7 +23,15 @@ class EuCentralBank < Money::Bank::VariableExchange
       io.each_line {|line| file.puts line}
     end
   end
-
+  
+  def update_rates_from_s(content)
+    update_parsed_rates(exchange_rates_from_s(content))
+  end
+  
+  def save_rates_to_s
+    open(ECB_RATES_URL).read
+  end
+    
   def exchange(cents, from_currency, to_currency)
     exchange_with(Money.new(cents, from_currency), to_currency)
   end
@@ -48,5 +53,19 @@ class EuCentralBank < Money::Bank::VariableExchange
     doc = Nokogiri::XML(open(rates_source))
     doc.xpath('gesmes:Envelope/xmlns:Cube/xmlns:Cube//xmlns:Cube')
   end
-
+  
+  def exchange_rates_from_s(content)
+    doc = Nokogiri::XML(content)
+    doc.xpath('gesmes:Envelope/xmlns:Cube/xmlns:Cube//xmlns:Cube')
+  end
+  
+  def update_parsed_rates(rates)
+    rates.each do |exchange_rate|
+      rate = exchange_rate.attribute("rate").value.to_f
+      currency = exchange_rate.attribute("currency").value
+      add_rate("EUR", currency, rate)
+    end
+    add_rate("EUR", "EUR", 1)
+    @last_updated = Time.now
+  end
 end
